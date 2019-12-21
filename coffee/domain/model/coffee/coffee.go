800b8b16a14724fe9ab37a3fb6ede97d1fe196c3
Eric.Huang <eric.g.yuan@gmail.com>
2019-12-22 02:31:57 +0800

    Add getters for coffee entity

--- coffee/domain/model/coffee/coffee.go
+++ coffee/domain/model/coffee/coffee.go
@@ -7,21 +7,37 @@ import (
 
 type Coffee struct {
 	ddd.AggregateRoot
-	id        ID
-	tableNo   string
-	items     []Item
-	createdAt time.Time
+	id          ID
+	tableNo     string
+	productName string
+	createdAt   time.Time
 }
 
-func Make(coffeeID ID, tableNo string, items []Item, createAt time.Time) *Coffee {
+func (c *Coffee) CreatedAt() time.Time {
+	return c.createdAt
+}
+
+func (c *Coffee) ProductName() string {
+	return c.productName
+}
+
+func (c *Coffee) TableNo() string {
+	return c.tableNo
+}
+
+func (c *Coffee) ID() ID {
+	return c.id
+}
+
+func Make(coffeeID ID, tableNo, productName string, createAt time.Time) *Coffee {
 
 	//TODO: Verify by policy
 	//TODO: Apply event
 
 	return &Coffee{
-		id:        coffeeID,
-		tableNo:   tableNo,
-		items:     items,
-		createdAt: createAt,
+		id:          coffeeID,
+		tableNo:     tableNo,
+		productName: productName,
+		createdAt:   createAt,
 	}
 }
